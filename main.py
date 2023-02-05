2fb10f9acfaf85b874a3db7d43af40e1da494176
iamwalshy <iamwalshy@protonmail.com>
2023-02-04 21:23:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+# Gaming Hardware section
+st.header("Gaming Hardware")
+st.write("This section showcases the best gaming hardware, including keyboards, mice, and headsets. Here, you can find detailed reviews and comparisons, and make informed decisions based on your budget and gaming style.")
+
+# Gaming Performance Optimization section
+st.header("Gaming Performance Optimization")
+st.write("This section provides resources for optimizing your gaming performance, including guides for configuring graphics settings, improving network speed, and reducing lag. Here, you'll also find tools and software that are specifically designed to help you get the most out of your gaming experience.")
+
+# Gaming Walkthroughs and Strategy Guides section
+st.header("Gaming Walkthroughs and Strategy Guides")
+st.write("This section offers walkthroughs and strategy guides for popular games, along with tips and tricks for beating difficult levels and mastering different characters. Whether you're a casual gamer or a hardcore pro, you'll find resources to help you get the most out of your gaming experience.")
+
+# Gaming Communities and Forums section
+st.header("Gaming Communities and Forums")
+st.write("This section highlights the best gaming communities and forums, and provides resources for finding and joining online gaming groups. Whether you're looking for players to join your game or simply want to chat with like-minded gamers, you'll find everything you need here.")
+
+# Streamers and Pro Gamers section
+st.header("Streamers and Pro Gamers")
+st.write("This section features profiles of top streamers and pro gamers, including their configurations, play styles, and tips for success. Whether you're looking to improve your own gaming skills or simply want to be inspired by the best, you'll find everything you need here.")
+
+# Improving Gaming Skills section
+st.header("Improving Gaming Skills")
+st.write("This section provides resources for improving your gaming skills, including training exercises, tutorials, and game-specific tips and tricks. Whether you're a beginner or a seasoned gamer, you'll find resources to help you take your skills to the next level.")
+
+# Latest and Greatest Games section
+st.header("Latest and Greatest Games")
+st.write("This section showcases the latest and greatest games, along with reviews, trailers, and links to download or purchase them. Whether you're looking for the hottest new releases or simply want to stay up-to-date on the latest trends in gaming, you'll find everything you need here.")
+
+# Cheats, Mods, and Custom Maps section
+st.header("Cheats, Mods, and Custom Maps")
+st.write("This section features a database of cheat codes, mods, and custom maps for various games, along with tutorials for using them effectively. Whether you're looking for an edge in your favorite game or simply want to explore new and exciting possibilities, you'll find everything you need here.")
+
+# Gaming Troubleshooting section
+st.header("Gaming Troubleshooting")
+st.write("This section provides resources for troubleshooting common gaming issues, including fixing crashes, improving frame rates, and resolving network problems. Whether you're facing technical difficulties or simply want to maintain optimal gaming performance, you'll find everything you need here.")
---
+++ sensitivityconverter.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def main():
+    st.title("Aiming.pro Mouse Sensitivity Converter")
+
+    # User Inputs
+    current_sensitivity = st.sidebar.number_input("Current Sensitivity", value=5.0, min_value=0.1, max_value=30.0, step=0.1)
+    current_zoom_sensitivity = st.sidebar.number_input("Current Zoom Sensitivity", value=2.5, min_value=0.1, max_value=30.0, step=0.1)
+    popular_games = ["CS:GO", "Fortnite", "Apex Legends", "Overwatch", "PUBG", "Rainbow Six Siege", "Valorant", "Call of Duty: Warzone", "Destiny 2", "Minecraft"]
+    current_game = st.sidebar.selectbox("Current Game", popular_games, index=0)
+    target_game = st.sidebar.selectbox("Target Game", popular_games, index=0)
+    mouse_dpi = st.sidebar.slider("Mouse DPI", 400, 16000, 800)
+
+    # Sensitivity Conversion
+    converted_sensitivity = current_sensitivity * mouse_dpi / 800
+    converted_zoom_sensitivity = current_zoom_sensitivity * mouse_dpi / 800
+
+    # Output
+    st.write("Your converted sensitivity is:", converted_sensitivity)
+    st.write("Your converted zoom sensitivity is:", converted_zoom_sensitivity)
+
+    st.write("From Game:", current_game)
+    st.write("To Game:", target_game)
+
+if __name__ == "__main__":
+    main()
